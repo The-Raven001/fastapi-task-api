c77b114c120b8d44912daa79147a40ae90736dd1
Gabriel <gacm95218@gmail.com>
2025-12-10 02:58:14 -0400

    Added security feature to endpoints so that only users can request, edit, create or delete their own tasks without being able to get tasks from other user

--- app/main.py
+++ app/main.py
@@ -15,7 +15,9 @@ def home():
     return {"message": "This is just the landing page"}
 
 
-#To start running this api you have to type in the console: uvicorn app.main:app --reload
+#To start running this api you have to type in the console:
+#1 .\venv\Scripts\activate 
+#2 uvicorn app.main:app --reload
 
 """
 examples:
--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -2,13 +2,23 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.database import get_db
 from sqlalchemy.orm import Session
 from app import models, schemas
+from app.routes.auth import get_current_user
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 #Create task
 @router.post("/", response_model=schemas.Task, status_code=201)
-def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    new_task = models.Task(**task.model_dump())
+def create_task(
+    task: schemas.TaskCreate, 
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user)):
+
+    new_task = models.Task(
+        title=task.title,
+        description=task.description,
+        completed=task.completed,
+        owner_id=current_user.id)
+
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
@@ -16,12 +26,19 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
 
 #Get tasks, all/individually
 @router.get("/", response_model=list[schemas.Task], status_code=201)
-def get_tasks(db: Session = Depends(get_db)):
-    return db.query(models.Task).all()
+def get_tasks(db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user)):
+    return db.query(models.Task).filter(models.Task.ownew_id == current_user.id).all()
 
 @router.get("/{id}", response_model=schemas.TaskBase)
-def get_task(id: int, db: Session = Depends(get_db)):
-    task = db.query(models.Task).filter(models.Task.id == id).first()
+def get_task(id: int, 
+            db: Session = Depends(get_db),
+            current_user: models.User = Depends(get_current_user)
+):
+    task = db.query(models.Task).filter(
+        models.Task.id == id,
+        models.Task.owner_id == current_user.id
+    ).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
@@ -29,8 +46,8 @@ def get_task(id: int, db: Session = Depends(get_db)):
 #Update task
 
 @router.put("/{id}", response_model=schemas.Task)
-def update_item(id: int, updated_task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    task = db.query(models.Task).filter(models.Task.id == id).first()
+def update_item(id: int, updated_task: schemas.TaskCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    task = db.query(models.Task).filter(models.Task.id == id, models.Task.owner_id == current_user.id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     task.title = updated_task.title
@@ -41,9 +58,11 @@ def update_item(id: int, updated_task: schemas.TaskCreate, db: Session = Depends
     db.refresh(task)
     return task
 
+#Delete task
+
 @router.delete("/{id}", response_model=schemas.Task)
-def delete_task(id: int, db: Session = Depends(get_db)):
-    task = db.query(models.Task).filter(models.Task.id == id).first()
+def delete_task(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    task = db.query(models.Task).filter(models.Task.id == id, models.Task.owner_id == current_user.id).first()
     if not task:
         raise HTTPException(status_code=404, detail="task not found")
     db.delete(task)
